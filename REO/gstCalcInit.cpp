7881ecfbab2ed2470e1500502b3b28c35fd47a6d
Alexey Stupishin <agstup@yandex.ru>
2024-12-02 04:00:35 +0300

    Major update

--- REO/gstCalcInit.cpp
+++ REO/gstCalcInit.cpp
@@ -1,7 +1,7 @@
 // MEX wrapper
 //
-// mex -v -largeArrayDims s:\Projects\Matlab\REO\gstCalcInit.cpp -outdir s:\Projects\Matlab\REO
-// mex -v -g -largeArrayDims s:\Projects\Matlab\REO\gstCalcInit.cpp -outdir s:\Projects\Matlab\REO
+// mex -v -largeArrayDims s:\Projects\Matlab\MatlabUtils\REO\gstCalcInit.cpp -outdir s:\Projects\Matlab\MatlabUtils\REO
+// mex -v -g -largeArrayDims s:\Projects\Matlab\MatlabUtils\REO\gstCalcInit.cpp -outdir s:\Projects\Matlab\MatlabUtils\REO
 //
 
 #include <Windows.h>
